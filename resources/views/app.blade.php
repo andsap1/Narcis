5a7cfcb42a8c2dc263303756b2bd6b925a2e3a1b
andsap1 <48027218+andsap1@users.noreply.github.com>
2020-11-12 23:44:23 +0200

    prekiu langas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,7 +24,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
 {{--        <a class="navbar-brand" href="#">Start Bootstrap</a>--}}
-        <img style="width: 250px; height: 100px" src="images/logo.png"  class="rounded float-left" alt="...">
+        <img style="width: 230px; height: 90px" src="images/final1.png"  class="rounded float-left" alt="...">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
