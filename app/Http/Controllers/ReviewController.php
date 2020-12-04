c0767ec6f49b5e8203fe686769af6e81e1d750b3
Aidbie <47851440+Aidbie@users.noreply.github.com>
2020-12-04 09:20:40 +0200

    null

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Redirect;
 class ReviewController extends Controller
 {
     public function reviews(){
-        $zero=0;
+        $zero=null;
         $items = Atsiliepimas::where('naudotojo_nuotraukos_pavadinimas','!=',$zero)->get();
 
 
@@ -31,7 +31,7 @@ class ReviewController extends Controller
         $atsiliepimai -> fk_naudotojasid_Naudotojas = $user->id;
 
         $atsiliepimai->save();
-        return Redirect::to('/atsiliepimai')->with('success', 'Atsiliepimas pridėtas!');
+        return Redirect::to('/atsiliepimai')->with('success', 'Atsiliepimas pridėtas! Laukiama patvirtinimo');
     }
 
     public function editReview($id){
